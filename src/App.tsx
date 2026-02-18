14a379f99f7f5a9af55f5bf9f79b7ca0274f5b2e
RevengerZ <99681425+RevengerZ@users.noreply.github.com>
2026-02-18 23:12:19 +0530

    Add 'hanshi-david' to sections array

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
   const handleScroll = () => {
-  const sections = ['home', 'about', 'achievements', 'events', 'hanshi', 'shihan', 'contact'];
+  const sections = ['home', 'about', 'achievements', 'events', 'hanshi', 'shihan','hanshi-david', 'contact'];
       const scrollPosition = window.scrollY + 100;
 
       for (const section of sections) {
